111bf79d1c72981df050dfb8493c049ac1736067
Phil Broadley <philbrocoding@hotmail.com>
2023-02-03 16:51:50 +0000

    update

---
+++ run_tests.py
@@ -0,0 +1,10 @@
+import unittest
+from tests.city_test import cityTest
+from tests.country_test import countryTest
+from tests.sight_test import sightTest
+from tests.user_test import userTest
+from tests.visit_test import visitTest
+
+
+if __name__ == '__main__':
+    unittest.main()
