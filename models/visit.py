21ff62c7f1e71f040cf4017b6b44e83b4e53a97f
Phil Broadley <philbrocoding@hotmail.com>
2023-02-03 13:11:18 +0000

    Visit class updated

---
+++ models/visit.py
@@ -0,0 +1,9 @@
+class Visit:
+    def __init__(self, user, city, visited = False, on_list = False, id = None):
+        self.user = user
+        self.city = city
+        self.visited = visited
+        self.on_list = on_list
+        self.id = id
+
+        
