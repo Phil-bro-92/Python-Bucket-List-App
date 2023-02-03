98ab170259f1c223fb6728837fabb2212db81258
Phil Broadley <philbrocoding@hotmail.com>
2023-02-03 13:04:56 +0000

    Country class updated

---
+++ models/country.py
@@ -0,0 +1,7 @@
+class Country:
+    def __init__(self, name, continent, population, id = None):
+        self.name = name
+        self.continent = continent
+        self.population = population
+        self.id = id
+
